94d922a94696e3ba18e05bcebdeb8b5741c9b2b8
Victoria Kachanovska <kach.vikt@gmail.com>
2017-05-17 13:00:31 +0300

    create new project

---
+++ src/earthquakes/programming/and/interfaces/EarthquakesProgrammingAndInterfaces.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package earthquakes.programming.and.interfaces;
+
+/**
+ *
+ * @author New
+ */
+public class EarthquakesProgrammingAndInterfaces {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
